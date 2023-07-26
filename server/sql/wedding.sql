a2c99f5f7cc111f40abf087739f162db367feb51
dr34-m <1416814478@qq.com>
2023-07-26 23:21:16 +0800

    update sql调整

--- server/sql/wedding.sql
+++ server/sql/wedding.sql
@@ -2,18 +2,28 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for conf
+-- ----------------------------
+DROP TABLE IF EXISTS `conf`;
+CREATE TABLE `conf`  (
+  `id` int(11) NOT NULL,
+  `status` tinyint(4) NULL DEFAULT 0 COMMENT '状态，0-未开奖，1-开奖中，2-开奖结束',
+  `key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '婚礼当天key',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '配置表' ROW_FORMAT = Compact;
+
+-- ----------------------------
 -- Table structure for userList
 -- ----------------------------
 DROP TABLE IF EXISTS `userList`;
 CREATE TABLE `userList`  (
-  `openId` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '微信openId',
-  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '头像',
-  `nickName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '昵称',
-  `num` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '抽奖号',
-  `status` tinyint(4) NULL DEFAULT 0 COMMENT '状态，0-未开奖，1-开奖中，2-中奖，3-未中奖',
-  `reword` tinyint(4) NULL DEFAULT 0 COMMENT '奖项，0-创建，1-一等奖，2-二等奖，3-三等奖，4-幸运奖',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `openId` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '微信openId',
+  `status` tinyint(4) NULL DEFAULT 0 COMMENT '状态，0-暂未中奖，1-中奖',
+  `reword` tinyint(4) NULL DEFAULT 0 COMMENT '奖项，0-暂未中奖，1-一等奖，2-二等奖，3-三等奖，4-幸运奖',
   `createTime` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
-  PRIMARY KEY (`openId`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户信息表' ROW_FORMAT = Compact;
+  `updateTime` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户信息表' ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
